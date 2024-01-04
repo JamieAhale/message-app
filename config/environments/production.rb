6b31264afa2c81fbac8a838338dc6df8b42d0e7f
Jamie Ahale <jamie.w.ahale@gmail.com>
2024-01-04 13:32:20 +1000

    heroku configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.cache_store = :redis_store, ENV['REDIS_URL'] || "redis://localhost:6379/0/cache"
+
+
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
